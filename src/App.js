7e545d66036f81c931b64dbc54becf428ea2fac5
catiemondon <Cathmond25@gmail.com>
2019-05-28 09:51:21 -0600

    completed first 3 animations and got mobile breakpoint

--- src/App.js
+++ src/App.js
@@ -5,21 +5,60 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+        <nav>
+                <span>
+                    <h1>Start Bootstrap</h1>
+                </span>
+
+                <ul className='desktop-navigation'>
+                    <li>Services</li>
+                    <li>Portfolio</li>
+                    <li>About</li>
+                    <li>Team</li>
+                    <li>Contact</li>
+                </ul>
+        <div className="mobilenav">
+         
+          
+          
+         
+        <div className="mobile-navigation">
+         
+          <div className="line"></div>
+          <div className="line"></div>
+          <div className="line"></div>
+        </div>
+
+        <div className="collapsible-menu">
+
+        <label for="menu">Menu</label>
+
+      <input type="checkbox" id="menu" />
+    <div className="menu-content">
+        <ul className="nav2">
+            <li>Services</li>
+            <li>Portfolio</li>
+            <li>About</li>
+            <li>Team</li>
+            <li>Contact</li>
+            
+         </ul>
+     </div>
+     
+
+     </div>
+</div>
+</nav>
+</div>
+
+
+      
+            
+    
+     
+
+
   );
 }
 
